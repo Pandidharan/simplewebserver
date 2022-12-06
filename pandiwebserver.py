f830e7b70253a793244840301d82f7e3ed5ccc05
Pandidharan <pandidharan22124@gmail.com>
2022-12-06 09:27:36 +0000

    Myweb server

---
+++ pandiwebserver.py
@@ -0,0 +1,23 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+content = '''
+<html>
+<head>
+</head>
+<body>
+<h1>Top web developement frameworks</h1>
+<h2>1.Django</h2>
+<h2>2.MEAN Stack</h2>
+</body>
+</html>
+'''
+
+class myserver(BaseHTTPRequestHandler):
+    def do_GET(self): 
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(content.encode())
+print("Get request recieved")
+
+server_address = ('',80)
+httpd = HTTPServer(server_address, myserver)
+httpd.serve_forever()
